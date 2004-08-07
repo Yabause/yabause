86bf9685b50052ee429e50161bbd58706e10064c
adamgreen <adamgreen@d7be59e3-070e-4b9a-85a1-4646befae34d>
2004-08-07 21:22:11 +0000

    CD-ROM support on OS X - not yet tested
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@66 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/macosx/cd.cc
+++ yabause/src/macosx/cd.cc
@@ -1,4 +1,5 @@
-/*  Copyright 2004 Theo Berkau
+/*  Copyright 2004 Lucas Newman
+	Copyright 2004 Theo Berkau
 
     This file is part of Yabause.
 
@@ -32,22 +33,45 @@
 
 char cdPath[1024];
 int hCDROM;
+CDTOC *cdTOC = NULL;
 
-int GetBSDPath( io_iterator_t mediaIterator, char *bsdPath, CFIndex maxPathSize )
+int getCDPath( io_iterator_t mediaIterator, char *devPath, CFIndex maxPathSize )
 {
     io_object_t nextMedia;
-    *bsdPath = '\0';
+	CFDictionaryRef properties;
+    CFDataRef data;
+    *devPath = '\0';
     
     nextMedia = IOIteratorNext( mediaIterator );
     if (nextMedia) {
-        CFTypeRef bsdPathAsCFString;
-        bsdPathAsCFString = IORegistryEntryCreateCFProperty(nextMedia, CFSTR(kIOBSDNameKey), kCFAllocatorDefault, 0);
-        if (bsdPathAsCFString) {
+		CFTypeRef devPathAsCFString;
+        devPathAsCFString = IORegistryEntryCreateCFProperty(nextMedia, CFSTR(kIOBSDNameKey), kCFAllocatorDefault, 0);
+        if (devPathAsCFString) {
             size_t devPathLength;
-            strcpy(bsdPath, "/dev/r");
-            devPathLength = strlen(bsdPath);
-            CFStringGetCString(bsdPathAsCFString,  bsdPath + devPathLength, maxPathSize - devPathLength, kCFStringEncodingASCII);
-            CFRelease(bsdPathAsCFString);
+            strcpy(devPath, "/dev/r");
+            devPathLength = strlen(devPath);
+            CFStringGetCString(devPathAsCFString,  devPath + devPathLength, maxPathSize - devPathLength, kCFStringEncodingASCII);
+            CFRelease(devPathAsCFString);
+			
+			if((IORegistryEntryCreateCFProperties(nextMedia, &properties, kCFAllocatorDefault, kNilOptions)) != 0)
+				perror("IORegistryEntryCreateCFProperties Failed!");
+			
+			if((data = (CFDataRef) CFDictionaryGetValue(properties, CFSTR(kIOCDMediaTOCKey))) != NULL) {
+				CFRange range;
+				CFIndex buf_len;
+
+				buf_len = CFDataGetLength( data ) + 1;
+				range = CFRangeMake( 0, buf_len );
+
+				if((cdTOC = (CDTOC *)malloc( buf_len )) != NULL)
+				{
+					CFDataGetBytes( data, range, (u_char *)cdTOC );
+				}
+			}
+			else {
+				perror("CFDictionaryGetValue Failed!");
+			}
+			CFRelease(properties);
         }
         IOObjectRelease(nextMedia);
     }
@@ -72,7 +96,7 @@ int CDInit(char *cdrom_name)
 	if (IOServiceGetMatchingServices(masterPort, classesToMatch, &mediaIterator) != 0)
 		printf("IOServiceGetMatchingServices Failed!\n");
 		
-	GetBSDPath(mediaIterator, cdPath, sizeof(cdPath));
+	getCDPath(mediaIterator, cdPath, sizeof(cdPath));
 	
 	if ((hCDROM = open(cdPath, O_RDONLY | O_NONBLOCK)) == -1)
 		return -1;
@@ -84,6 +108,7 @@ int CDDeInit()
 {
 	if (hCDROM != -1) {
 		close(hCDROM);
+		free((void*)cdTOC);
 		printf("CDDeInit OK\n");
 	}
 	return 0;
@@ -99,10 +124,43 @@ bool CDIsCDPresent()
 
 long CDReadToc(unsigned long *TOC)
 {
-   return 0;
+    int add150 = 0;
+	u_char track;
+    int i, lba = 0; 
+    CDTOCDescriptor *pTrackDescriptors;
+    pTrackDescriptors = cdTOC->descriptors;
+	
+	memset(TOC, 0xFF, 0xCC * 2);
+	
+	/* First three entries are first, last, and lead out - track one starts at [3] */
+	TOC[0] = (pTrackDescriptors[3].control << 28 |
+		pTrackDescriptors[3].adr << 24 | CDConvertMSFToLBA(pTrackDescriptors[3].p));
+		
+	if (CDConvertMSFToLBA(pTrackDescriptors[3].p) == 0) add150 = 150;
+		
+	/* Convert TOC to Saturn format */
+	for( i = 0; i < CDTOCGetDescriptorCount(cdTOC); i++ ) {
+        track = pTrackDescriptors[i].point;
+		lba = CDConvertMSFToLBA(pTrackDescriptors[i].p) + add150;
+		
+        if ((track > 99) || (track < 1))
+            continue;
+		TOC[i] = (pTrackDescriptors[3].control << 28 | 
+			pTrackDescriptors[3].adr << 24 | CDConvertMSFToLBA(pTrackDescriptors[3].p));
+    }
+	
+	/* Do first, last, and lead out */
+	for( i = 0; i < 3; i++ ) {
+	TOC[99+i] = (pTrackDescriptors[i].control << 28 | 
+			pTrackDescriptors[i].adr << 24 | CDConvertMSFToLBA(pTrackDescriptors[i].p));
+	}
+	
+	return (0xCC * 2);
 }
 
 unsigned long CDReadSector(unsigned long lba, unsigned long size, void *buffer)
 {
-   return 0;
+    if (hCDROM != -1)
+		return pread(hCDROM, buffer, size, lba * 2048);
+    return 0;
 }
