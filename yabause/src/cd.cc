27b13ebbc28f94ab0172d1c0b5e35f393c78a2aa
cyberwarriorx <cyberwarriorx@d7be59e3-070e-4b9a-85a1-4646befae34d>
2004-08-10 01:45:01 +0000

    -Added a bunch of notes in regards to how to setup the TOC.
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@70 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/cd.cc
+++ yabause/src/cd.cc
@@ -36,6 +36,32 @@ bool CDIsCDPresent()
 
 long CDReadToc(unsigned long *TOC)
 {
+   // The format of TOC is as follows:
+   // TOC[0] - TOC[98] are meant for tracks 1-99. Each entry has the following
+   // format:
+   // bits 0 - 23: track FAD address
+   // bits 24 - 27: track addr
+   // bits 28 - 31: track ctrl
+   //
+   // Any Unused tracks should be set to 0xFFFFFFFF
+   //
+   // TOC[99] uses the following format:
+   // bits 16 - 23: first track's number
+   // bits 24 - 27: first track's addr
+   // bits 28 - 31: first track's ctrl
+   //
+   // TOC[100] uses the following format:
+   // bits 16 - 23: last track's number
+   // bits 24 - 27: last track's addr
+   // bits 28 - 31: last track's ctrl
+   //
+   // TOC[101] uses the following format:
+   // bits 0 - 23: leadout FAD address
+   // bits 24 - 27: leadout's addr
+   // bits 28 - 31: leadout's ctrl
+   //
+   // Special Note: To convert from LBA to FAD, add 150.
+
    return 0;
 }
 
