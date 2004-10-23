6809e84311890d2633bcffffa3f82be0b8d5feb0
cyberwarriorx <cyberwarriorx@d7be59e3-070e-4b9a-85a1-4646befae34d>
2004-10-23 20:20:17 +0000

    -Added the new interface functions
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@119 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/windows/cd.cc
+++ yabause/src/windows/cd.cc
@@ -66,6 +66,79 @@ bool CDIsCDPresent()
    return success;
 }
 
+int CDGetStatus()
+{
+   // This function is called periodically to see what the status of the
+   // drive is.
+   //
+   // Should return one of the following values:
+   // 0 - CD Present, disc spinning
+   // 1 - CD Present, disc not spinning
+   // 2 - CD not present
+   // 3 - Tray open
+   //
+   // If you really don't want to bother too much with this function, just
+   // return status 0. Though it is kind of nice when the bios's cd player,
+   // etc. recognizes when you've ejected the tray and popped in another disc.
+   DWORD dwBytesReturned;
+   unsigned char statusbuf[8];
+   BOOL success;
+
+   sptd.Length=sizeof(sptd);
+   sptd.PathId=0;   //
+   sptd.TargetId=0; // Don't need these, they're automatically generated
+   sptd.Lun=0;      //
+   sptd.CdbLength=12;
+   sptd.SenseInfoLength=0; // No sense data
+   sptd.DataIn=SCSI_IOCTL_DATA_IN; 
+   sptd.TimeOutValue=60; // may need to be changed
+   sptd.DataBuffer=(PVOID)&(statusbuf);
+   sptd.SenseInfoOffset=0;
+   sptd.DataTransferLength=8; // may need to change this
+
+   sptd.Cdb[0]=0xBD; // CDB12 code
+   sptd.Cdb[1]=0; // Reserved
+   sptd.Cdb[2]=0; // Reserved
+   sptd.Cdb[3]=0; // Reserved
+   sptd.Cdb[4]=0; // Reserved
+   sptd.Cdb[5]=0; // Reserved
+   sptd.Cdb[6]=0; // Reserved
+   sptd.Cdb[7]=0; // Reserved
+   sptd.Cdb[8]=0; // Allocation Length(byte 1)
+   sptd.Cdb[9]=8; // Allocation Length(byte 2) (may have to change this)
+   sptd.Cdb[10]=0; // Reserved
+   sptd.Cdb[11]=0; // Control
+   sptd.Cdb[12]=0;
+   sptd.Cdb[13]=0;
+   sptd.Cdb[14]=0;
+   sptd.Cdb[15]=0;
+
+   success=DeviceIoControl(hCDROM,
+                           IOCTL_SCSI_PASS_THROUGH_DIRECT,
+                           (PVOID)&sptd, (DWORD)sizeof(SCSI_PASS_THROUGH_DIRECT),
+                           NULL, 0,
+                           &dwBytesReturned,
+                           NULL);
+
+   if (success)
+   {
+      // Figure out drive status
+
+      // Is door open?
+      if (statusbuf[1] & 0x10) return 3;
+
+      // Ok, so the door is closed, now is there a disc there?
+      success = DeviceIoControl(hCDROM, IOCTL_STORAGE_CHECK_VERIFY,
+                                NULL, 0, NULL, 0, &dwBytesReturned, NULL);
+      if (!success)
+         return 2;
+
+      return 0;
+   }
+
+   return 2;
+}
+
 #define MSF_TO_FAD(m,s,f) ((m * 4500) + (s * 75) + f)
 
 long CDReadToc(unsigned long *TOC)
@@ -87,8 +160,6 @@ long CDReadToc(unsigned long *TOC)
       {
          return 0;
       }
-
-      printf("cd:\tToc info: First Track = %d Last Track = %d\n", ctTOC.FirstTrack, ctTOC.LastTrack);
 
       // convert TOC to saturn format
       for (i = 0; i < ctTOC.LastTrack; i++)
@@ -112,12 +183,6 @@ long CDReadToc(unsigned long *TOC)
                  (ctTOC.TrackData[ctTOC.LastTrack].Adr << 24) |
                   MSF_TO_FAD(ctTOC.TrackData[ctTOC.LastTrack].Address[1], ctTOC.TrackData[ctTOC.LastTrack].Address[2], ctTOC.TrackData[ctTOC.LastTrack].Address[3]);
 
-//      if ((debugfp = fopen("toc.out", "wb")) != NULL)
-//      {
-//        fwrite((void *)TOC, 1, 2 * 0xCC, debugfp);
-//        fclose(debugfp);
-//      }
-
       return (0xCC * 2);
    }
 
@@ -131,12 +196,6 @@ unsigned long CDReadSector(unsigned long lba, unsigned long size, void *buffer)
 
    if (hCDROM != INVALID_HANDLE_VALUE)
    {
-/*      char tempstr[128];
-
-      sprintf(tempstr, "type = %d lba = %d size = %d\n", type, lba, size);
-      MessageBox (NULL, tempstr, "Notice",
-                  MB_OK | MB_ICONINFORMATION);
-
       sptd.Length=sizeof(sptd);
       sptd.PathId=0;   // Don't need these, they're automatically generated
       sptd.TargetId=0;
@@ -145,7 +204,7 @@ unsigned long CDReadSector(unsigned long lba, unsigned long size, void *buffer)
       sptd.SenseInfoLength=0; // No sense data
       sptd.DataIn=SCSI_IOCTL_DATA_IN;
       sptd.TimeOutValue=60;  // may need to be changed
-      sptd.DataBuffer=(PVOID)&(buffer);
+      sptd.DataBuffer=(PVOID)buffer;
       sptd.SenseInfoOffset=0;
       sptd.DataTransferLength=size; 
 
@@ -175,13 +234,72 @@ unsigned long CDReadSector(unsigned long lba, unsigned long size, void *buffer)
                               NULL, 0,
                               &dwBytesReturned,
                               NULL);
-*/
 
-      SetFilePointer(hCDROM, lba * 2048, 0, FILE_BEGIN);
-      success = ReadFile(hCDROM, buffer, size, &dwBytesReturned, NULL);
-
-      return dwBytesReturned;
+      // fix me
+      return 2048;
    }
 
    return 0;
 }
+
+bool CDReadSectorFAD(unsigned long FAD, void *buffer)
+{
+   // This function is supposed to read exactly 1 -RAW- 2352-byte sector at
+   // the specified FAD address to buffer. Should return true if successful,
+   // false if there was an error.
+   //
+   // Special Note: To convert from FAD to LBA/LSN, minus 150.
+   //
+   // The whole process needed to be changed since I need more control over
+   // sector detection, etc. Not to mention it means less work for the porter
+   // since they only have to implement raw sector reading as opposed to
+   // implementing mode 1, mode 2 form1/form2, -and- raw sector reading.
+
+   DWORD dwBytesReturned;
+   BOOL success;
+
+   sptd.Length=sizeof(sptd);
+   sptd.PathId=0;   //
+   sptd.TargetId=0; // Don't need these, they're automatically generated
+   sptd.Lun=0;      //
+   sptd.CdbLength=12;
+   sptd.SenseInfoLength=0; // No sense data
+   sptd.DataIn=SCSI_IOCTL_DATA_IN;
+   sptd.TimeOutValue=60; // may need to be changed
+   sptd.DataBuffer=(PVOID)buffer;
+   sptd.SenseInfoOffset=0;
+   sptd.DataTransferLength=2352; 
+
+   sptd.Cdb[0]=0xBE; // CDB12 code
+   sptd.Cdb[1]=0; // Sector Type, RELADR
+   FAD -= 150;
+   sptd.Cdb[2]=(unsigned char)((FAD & 0xFF000000) >> 24); // lba(byte 1)
+   sptd.Cdb[3]=(unsigned char)((FAD & 0x00FF0000) >> 16); // lba(byte 2)
+   sptd.Cdb[4]=(unsigned char)((FAD & 0x0000FF00) >> 8);  // lba(byte 3)
+   sptd.Cdb[5]=(unsigned char)(FAD & 0x000000FF);         // lba(byte 4)
+   sptd.Cdb[6]=0; // number of sectors(byte 1)
+   sptd.Cdb[7]=0; // number of sectors(byte 2)
+   sptd.Cdb[8]=1; // number of sectors(byte 3)
+   sptd.Cdb[9]=0xF8; // Sync + All Headers + User data + EDC/ECC
+   sptd.Cdb[10]=0;
+   sptd.Cdb[11]=0;
+   sptd.Cdb[12]=0;
+   sptd.Cdb[13]=0;
+   sptd.Cdb[14]=0;
+   sptd.Cdb[15]=0;
+
+   success=DeviceIoControl(hCDROM,
+                           IOCTL_SCSI_PASS_THROUGH_DIRECT,
+                           (PVOID)&sptd, (DWORD)sizeof(SCSI_PASS_THROUGH_DIRECT),
+                           NULL, 0,
+                           &dwBytesReturned,
+                           NULL);
+
+   if (!success)
+   {
+      return false;
+   }
+
+   return true;
+}
+
