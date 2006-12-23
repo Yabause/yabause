1bce101984ae75bacdb1ca5ea1b160ae4a8c042a
ljsebald <ljsebald@d7be59e3-070e-4b9a-85a1-4646befae34d>
2006-12-23 15:19:31 +0000

    Updating the build_dmg.sh script to include documentation files.
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1070 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/carbon/build_dmg.sh
+++ yabause/src/carbon/build_dmg.sh
@@ -2,6 +2,10 @@ hdiutil create -megabytes 20 tmp.dmg -layout NONE -fs HFS+ -volname Yabause -ov
 tmp=`hdid tmp.dmg`
 disk=`echo $tmp | cut -f 1 -d\ `
 cp -R Yabause.app /Volumes/Yabause/
+cp ../../ChangeLog /Volumes/Yabause/
+cp ../../README.MAC /Volumes/Yabause/
+cp ../../AUTHORS /Volumes/Yabause/
+cp ../../COPYING /Volumes/Yabause/
 hdiutil eject $disk
 hdiutil convert -format UDZO tmp.dmg -o Yabause.dmg
 rm tmp.dmg
