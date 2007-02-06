d8be7782f073341dcca3dfee96275e8f90fb2d99
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2007-02-06 21:35:26 +0000

    Fixed the segfault that occured when opening the preferences dialog on gtk port.
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1090 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/gtk/yuirange.c
+++ yabause/src/gtk/yuirange.c
@@ -130,9 +130,8 @@ GtkWidget * yui_range_new(GKeyFile * keyfile, const gchar * group, const gchar *
 		i++;
 	}
 	if ( !current ) {
-	  gtk_combo_box_set_active(GTK_COMBO_BOX(yfe->combo), (gint)(yfe->items[i].value) );
-	  g_key_file_set_value(yfe->keyfile, yfe->group, yfe->key,
-			       yfe->items[(gint)(yfe->items[i].value)].value);
+	  gtk_combo_box_set_active(GTK_COMBO_BOX(yfe->combo), 0);
+	  g_key_file_set_value(yfe->keyfile, yfe->group, yfe->key, "0");
 	}
 
         g_signal_connect(GTK_COMBO_BOX(yfe->combo), "changed", G_CALLBACK(yui_range_changed), yfe);
