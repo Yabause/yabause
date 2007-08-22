922f908d178b3f03edb7467b8681ee98a030e42c
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2007-08-22 20:09:18 +0000

    Moved the ini file to conform to XDG specification and updated ChangeLog.
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1198 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/gtk/main.c
+++ yabause/src/gtk/main.c
@@ -203,7 +203,22 @@ int main(int argc, char *argv[]) {
 #endif
 	LogStart();
 	LogChangeOutput( DEBUG_CALLBACK, YuiErrorMsg );
-	inifile = g_build_filename(g_get_home_dir(), ".yabause.ini", NULL);
+	inifile = g_build_filename(g_get_user_config_dir(), "yabause.ini", NULL);
+
+	if (! g_file_test(inifile, G_FILE_TEST_EXISTS)) {
+		// no inifile found, but it could in the old location
+		gchar * oldinifile;
+
+		oldinifile = g_build_filename(g_get_home_dir(), ".yabause.ini", NULL);
+
+		if (g_file_test(oldinifile, G_FILE_TEST_EXISTS)) {
+			// ok, we found an old .ini file, let's copy the content
+			gchar * data;
+
+			g_file_get_contents(oldinifile, &data, NULL, NULL);
+			g_file_set_contents(inifile, data, -1, NULL);
+		}
+	}
 	
 	keyfile = g_key_file_new();
 
