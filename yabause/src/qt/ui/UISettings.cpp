f436c923f5d89c58b8f9a83f7b63a40b262df235
pasnox <pasnox@d7be59e3-070e-4b9a-85a1-4646befae34d>
2008-02-03 23:13:57 +0000

    UISettings class is finished ( gtk clone ) Maybe it will need change to be compatible with the gtk ini file ?
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1372 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/qt/ui/UISettings.cpp
+++ yabause/src/qt/ui/UISettings.cpp
@@ -1,5 +1,12 @@
 #include "UISettings.h"
-#include "QtYabause.h"
+
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QInputDialog>
+#include <QDir>
+#include <QList>
+#include <QTimer>
+#include <QTime>
 
 extern M68K_struct* M68KCoreList[];
 extern SH2Interface_struct* SH2CoreList[];
@@ -8,9 +15,51 @@ extern CDInterface* CDCoreList[];
 extern SoundInterface_struct* SNDCoreList[];
 extern VideoInterface_struct* VIDCoreList[];
 
+struct Item
+{
+	Item( const QString& k, const QString& s )
+	{ id = k; Name = s; }
+	
+	QString id;
+	QString Name;
+};
+
+typedef QList<Item> Items;
+
+const Items mRegions = Items()
+	<< Item( "Auto" , "Auto-detect" )
+	<< Item( "J" , "Japan" )
+	<< Item( "T", "Asia" )
+	<< Item( "U", "North America" )
+	<< Item( "B", "Central/South America" )
+	<< Item( "K", "Korea" )
+	<< Item( "A", "Asia" )
+	<< Item( "E", "Europe + others" )
+	<< Item( "L", "Central/South America" );
+
+const Items mCartridgeTypes = Items()
+	<< Item( "0", "None" )
+	<< Item( "1", "Pro Action Replay" )
+	<< Item( "2", "4 Mbit Backup Ram" )
+	<< Item( "3", "8 Mbit Backup Ram" )
+	<< Item( "4", "16 Mbit Backup Ram" )
+	<< Item( "5", "32 Mbit Backup Ram" )
+	<< Item( "6", "8 Mbit Dram" )
+	<< Item( "7", "32 Mbit Dram" )
+	<< Item( "8", "Netlink" )
+	<< Item( "9", "16 Mbit ROM" );
+
+const Items mVideoFromats = Items()
+	<< Item( "0", "NTSC" )
+	<< Item( "1", "PAL" );
+
+const int TIMEOUT_SCAN = 1000;
+
 UISettings::UISettings( QWidget* p )
 	: QDialog( p )
 {
+	mScanningInput = false;
+	
 	// setup dialog
 	setupUi( this );
 
@@ -19,38 +68,174 @@ UISettings::UISettings( QWidget* p )
 
 	// load settings
 	loadSettings();
+	
+	// create timer for input scan
+	QTimer* mTimerInputScan = new QTimer( this );
+	mTimerInputScan->setInterval( TIMEOUT_SCAN / 2 );
+	
+	// connections
+	foreach ( QToolButton* tb, findChildren<QToolButton*>() )
+		connect( tb, SIGNAL( clicked() ), this, SLOT( tbBrowse_clicked() ) );
+	connect( mTimerInputScan, SIGNAL( timeout() ), this, SLOT( inputScan_timeout() ) );
+	
+	// start input scan
+	mTimerInputScan->start();
 }
 
 UISettings::~UISettings()
 {}
 
+void UISettings::qSleep( int ms )
+{
+    Q_ASSERT( ms > 0 );
+#ifdef Q_OS_WIN
+    Sleep( uint( ms ) );
+#else
+    struct timespec ts = { ms /1000, ( ms %1000 ) *1000 *1000 };
+    nanosleep( &ts, NULL );
+#endif
+}
+
+void UISettings::requestFile( const QString& c, QLineEdit* e )
+{
+	const QString s = QFileDialog::getOpenFileName( window(), c, e->text() );
+	if ( !s.isNull() )
+		e->setText( s );
+}
+
+void UISettings::requestFolder( const QString& c, QLineEdit* e )
+{
+	const QString s = QFileDialog::getExistingDirectory( window(), c, e->text() );
+	if ( !s.isNull() )
+		e->setText( s );
+}
+
+void UISettings::requestDrive( const QString& c, QLineEdit* e )
+{
+	// get all drives
+	QStringList drives;
+	foreach ( const QFileInfo& fi, QDir::drives() )
+		drives << fi.fileName();
+	
+	bool b;
+	const QString s = QInputDialog::getItem( window(), tr( "CD Rom Drive..." ), c, drives, 0, false, &b );
+	if ( b && !s.isNull() )
+		e->setText( s );
+}
+
+void UISettings::inputScan_timeout()
+{
+	// cancel if already listening input
+	if ( mScanningInput )
+		return;
+	
+	// get focused lineedit
+	QLineEdit* le = qobject_cast<QLineEdit*>( QApplication::focusWidget() );
+	if ( !le || le->parent() != wInput )
+		return;
+	
+	// get per core id
+	int i = PERCORE_DUMMY;
+#ifdef HAVE_LIBSDL && USENEWPERINTERFACE
+	i = PERCORE_SDLJOY;
+#endif
+	
+	// get percore pointer
+	PerInterface_struct* c = QtYabause::getPERCore( i );
+	
+	// check if we have valid core & can scan keys
+	if ( !c || c->canScan != 1 )
+		return;
+	
+	// tell we are listening input
+	mScanningInput = true;
+	
+	// get pressed input for key stock in the lineedit->statusTip()
+	const char* ki = strdup( le->statusTip().toAscii().constData() );
+	u32 k = 0;
+	QTime t;
+	t.start();
+	do
+	{
+		c->Flush();
+		k = c->Scan( ki );
+		QCoreApplication::processEvents( QEventLoop::AllEvents, TIMEOUT_SCAN );
+		qSleep( 10 );
+	} while ( t.elapsed() < TIMEOUT_SCAN && k == 0 && QApplication::focusWidget() == le );
+	
+	// write it in the settings dialog
+	if ( k != 0 )
+		le->setText( QString::number( k ) );
+	
+	// tell we no longer listening input
+	mScanningInput = false;
+}
+
+void UISettings::tbBrowse_clicked()
+{
+	// get toolbutton sender
+	QToolButton* tb = qobject_cast<QToolButton*>( sender() );
+	
+	if ( tb == tbBios )
+		requestFile( tr( "Choose a bios file" ), leBios );
+	else if ( tb == tbCdRom )
+	{
+		if ( cbCdRom->currentText().contains( "dummy", Qt::CaseInsensitive ) )
+		{
+			QMessageBox::information( window(), tr( "Informations..." ), tr( "The dummies cores don't need configuration." ) );
+			return;
+		}
+		else if ( cbCdRom->currentText().contains( "iso", Qt::CaseInsensitive ) )
+			requestFile( tr( "" ), leCdRom );
+		else
+		{
+#ifdef Q_OS_WIN
+			requestDrive( tr( "Choose a cdrom drive" ), leCdRom );
+#else
+			requestFolder( tr( "Choose a cdrom mount point" ), leCdRom );
+#endif
+		}
+	}
+	else if ( tb == tbSaveStates )
+		requestFolder( tr( "Choose a folder to store save states" ), leSaveStates );
+	else if ( tb == tbCartridge )
+	{
+		QMessageBox::information( window(), tr( "Informations..." ), tr( "Not yet implemented" ) );
+		return;
+	}
+	else if ( tb == tbMemory )
+		requestFile( tr( "Choose a memory file" ), leMemory );
+	else if ( tb == tbMpegROM )
+		requestFile( tr( "Choose a mpeg rom" ), leMpegROM );
+}
+
 void UISettings::loadCores()
 {
-	// CDInterface
+	// CD Drivers
 	for ( int i = 0; CDCoreList[i] != NULL; i++ )
 		cbCdRom->addItem( CDCoreList[i]->Name, CDCoreList[i]->id );
 	
-	// VDIInterface
+	// VDI Drivers
 	for ( int i = 0; VIDCoreList[i] != NULL; i++ )
 		cbVideoCore->addItem( VIDCoreList[i]->Name, VIDCoreList[i]->id );
 	
-	// VDIFormatInterface
-	/*
-	for ( int i = 0; VIDCoreList[i] != NULL; i++ )
-		cbVideoFormat->addItem( VIDCoreList[i]->Name, VIDCoreList[i]->id );
-	*/
+	// Video Formats
+	foreach ( const Item& it, mVideoFromats )
+		cbVideoFormat->addItem( it.Name, it.id );
 	
-	// SNDInterface
+	// SND Drivers
 	for ( int i = 0; SNDCoreList[i] != NULL; i++ )
 		cbSoundCore->addItem( SNDCoreList[i]->Name, SNDCoreList[i]->id );
 	
-	// Cartridge
-	//cbCartridge
+	// Cartridge Types
+	foreach ( const Item& it, mCartridgeTypes )
+		cbCartridge->addItem( it.Name, it.id );
 	
-	// Region
-	//cbRegion
+	// Regions
+	foreach ( const Item& it, mRegions )
+		cbRegion->addItem( it.Name, it.id );
 	
-	// Interpreters
+	// SH2 Interpreters
 	for ( int i = 0; SH2CoreList[i] != NULL; i++ )
 		cbSH2Interpreter->addItem( SH2CoreList[i]->Name, SH2CoreList[i]->id );
 }
@@ -77,14 +262,14 @@ void UISettings::loadSettings()
 	cbSoundCore->setCurrentIndex( cbSoundCore->findData( s->value( "Sound/SoundCore" ).toInt() ) );
 
 	// cartridge/memory
-	/*
-	cbCartridge
-	leCartridge->
-	leMemory->
-	leMpegROM->
-	*/
+	cbCartridge->setCurrentIndex( cbCartridge->findData( s->value( "Cartridge/Type" ).toInt() ) );
+	leCartridge->setText( s->value( "Cartridge/Path" ).toString() );
+	leMemory->setText( s->value( "Memory/Path" ).toString() );
+	leMpegROM->setText( s->value( "MpegROM/Path" ).toString() );
 	
 	// input
+	foreach ( QLineEdit* le, wInput->findChildren<QLineEdit*>() )
+		le->setText( s->value( QString( "Input/%1" ).arg( le->statusTip() ) ).toString() );
 	
 	// advanced
 	cbRegion->setCurrentIndex( cbRegion->findData( s->value( "Advanced/Region" ).toString() ) );
@@ -113,14 +298,14 @@ void UISettings::saveSettings()
 	s->setValue( "Sound/SoundCore", cbSoundCore->itemData( cbSoundCore->currentIndex() ).toInt() );
 
 	// cartridge/memory
-	/*
-	cbCartridge
-	leCartridge->
-	leMemory->
-	leMpegROM->
-	*/
+	s->setValue( "Cartridge/Type", cbCartridge->itemData( cbCartridge->currentIndex() ).toInt() );
+	s->setValue( "Cartridge/Path", leCartridge->text() );
+	s->setValue( "Memory/Path", leMemory->text() );
+	s->setValue( "MpegROM/Path", leMpegROM->text() );
 	
 	// input
+	foreach ( QLineEdit* le, wInput->findChildren<QLineEdit*>() )
+		s->setValue( QString( "Input/%1" ).arg( le->statusTip() ), le->text() );
 	
 	// advanced
 	s->setValue( "Advanced/Region", cbRegion->itemData( cbRegion->currentIndex() ).toString() );
--- yabause/src/qt/ui/UISettings.h
+++ yabause/src/qt/ui/UISettings.h
@@ -2,6 +2,7 @@
 #define UISETTINGS_H
 
 #include "ui_UISettings.h"
+#include "QtYabause.h"
 
 class UISettings : public QDialog, public Ui::UISettings
 {
@@ -12,11 +13,18 @@ public:
 	~UISettings();
 
 protected:
+	bool mScanningInput;
+	void qSleep( int ms );
+	void requestFile( const QString& caption, QLineEdit* edit );
+	void requestFolder( const QString& caption, QLineEdit* edit );
+	void requestDrive( const QString& caption, QLineEdit* edit );
 	void loadCores();
 	void loadSettings();
 	void saveSettings();
 
 protected slots:
+	void inputScan_timeout();
+	void tbBrowse_clicked();
 	void accept();
 };
 
