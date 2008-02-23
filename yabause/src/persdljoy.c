f28235a5545e2a9bf7a3970542874520de7943e1
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2008-02-23 21:54:19 +0000

    Merged PasNox modifications to SDL joystick core into persdljoy... in fact, I mostly just moved PerQtSDL.c code into persdljoy.c
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1440 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/persdljoy.c
+++ yabause/src/persdljoy.c
@@ -1,5 +1,6 @@
 /*  Copyright 2005 Guillaume Duhamel
     Copyright 2005-2006 Theo Berkau
+    Copyright 2008 Filipe Azevedo
 
     This file is part of Yabause.
 
@@ -28,146 +29,198 @@
 #include "debug.h"
 #include "persdljoy.h"
 
-int PERSDLJoyInit(void);
-void PERSDLJoyDeInit(void);
-int PERSDLJoyHandleEvents(void);
-void PERSDLJoyNothing(void);
+SDL_Joystick* mSDLJoystick1 = 0;
+Sint16* mSDLNeutralPoints = 0; // stock each axis neutral point
+int mCalibrationDone = 0; // use to check if autocalibration already done
+
+int PERQtSDLInit(void);
+void PERQtSDLDeInit(void);
+int PERQtSDLHandleEvents(void);
+void PERQtSDLNothing(void);
 #ifdef USENEWPERINTERFACE
-PortData_struct *PERSDLJoyGetPerDataP1(void);
-PortData_struct *PERSDLJoyGetPerDataP2(void);
+PortData_struct *PERQtSDLGetPerDataP1(void);
+PortData_struct *PERQtSDLGetPerDataP2(void);
 
 static PortData_struct port1;
 static PortData_struct port2;
 
-u32 PERSDLJoyScan(const char *);
-void PERSDLJoyFlush(void);
+u32 PERQtSDLScan(const char *);
+void PERQtSDLFlush(void);
 #endif
 
 PerInterface_struct PERSDLJoy = {
 PERCORE_SDLJOY,
-"SDL Joystick Interface",
-PERSDLJoyInit,
-PERSDLJoyDeInit,
+"Qt SDL Interface",
+PERQtSDLInit,
+PERQtSDLDeInit,
 #ifndef USENEWPERINTERFACE
-PERSDLJoyHandleEvents
+PERQtSDLHandleEvents
 #else
-PERSDLJoyHandleEvents,
-PERSDLJoyGetPerDataP1,
-PERSDLJoyGetPerDataP2,
-PERSDLJoyNothing,
-PERSDLJoyScan,
+PERQtSDLHandleEvents,
+PERQtSDLGetPerDataP1,
+PERQtSDLGetPerDataP2,
+PERQtSDLNothing,
+PERQtSDLScan,
 1,
-PERSDLJoyFlush
+PERQtSDLFlush
 #endif
 };
 
-typedef union {
-	u32 pack;
-	struct {
-		u8 type;
-		u8 key;
-		u16 value;
-	} data;
-} persdljoykey_struct;
-
 //////////////////////////////////////////////////////////////////////////////
 
-int PERSDLJoyInit(void) {
-   PerUpReleased();
-   PerDownReleased();
-   PerRightReleased();
-   PerLeftReleased();
-   PerStartReleased();
-   PerAReleased();
-   PerBReleased();
-   PerCReleased();
-   PerXReleased();
-   PerYReleased();
-   PerZReleased();
-   PerRTriggerReleased();
-   PerLTriggerReleased();
-
-   putenv("SDL_VIDEODRIVER=dummy");
-   SDL_InitSubSystem(SDL_INIT_JOYSTICK | SDL_INIT_VIDEO);
-   SDL_JoystickOpen(0);
-
-   return 0;
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-void PERSDLJoyDeInit(void) {
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-void PERSDLJoyNothing(void) {
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-int PERSDLJoyHandleEvents(void) {
-   SDL_Event event;
-   persdljoykey_struct k;
-   static u16 lastvalue;
-
-   k.pack = 0;
-
-   while(SDL_PollEvent(&event)) {
-      k.data.type = event.type;
-      switch(event.type) {
-		case SDL_JOYAXISMOTION:
-			k.data.key = event.jaxis.axis;
-			if (event.jaxis.value == 0) {
-				k.data.value = lastvalue;
-				//PERSDLJoyKeyUp(k.pack);
-				PerKeyUp(k.pack);
-			} else {
-				k.data.value = event.jaxis.value;
-				lastvalue = k.data.value;
-				//PERSDLJoyKeyDown(k.pack);
-				PerKeyDown(k.pack);
-			}
-			break;
-		case SDL_JOYBUTTONDOWN:
-			k.data.key = event.jbutton.button;
-			//PERSDLJoyKeyDown(k.pack);
-			PerKeyDown(k.pack);
-			break;
-		case SDL_JOYBUTTONUP:
-      			k.data.type = SDL_JOYBUTTONDOWN;
-			k.data.key = event.jbutton.button;
-			//PERSDLJoyKeyUp(k.pack);
-			PerKeyUp(k.pack);
-			break;
-		default:
-			return 0;
+int PERQtSDLInit(void) {
+	if ( mSDLJoystick1 )
+		return 0;
+	// init joysticks
+	if ( SDL_InitSubSystem( SDL_INIT_JOYSTICK ) == -1 )
+		return -1;
+	// ignore joysticks event in sdl event loop
+	SDL_JoystickEventState( SDL_IGNORE );
+	// open first joystick
+	mSDLJoystick1 = SDL_JoystickOpen( 0 );
+	// is it open ?
+	if ( !mSDLJoystick1 )
+	{
+		PERQtSDLDeInit();
+		return -1;
 	}
-   }
-   if (YabauseExec() != 0)
-      return -1;
-   if (YabauseExec() != 0)
-      return -1;
-   if (YabauseExec() != 0)
-      return -1;
-   if (YabauseExec() != 0)
-      return -1;
-   if (YabauseExec() != 0)
-      return -1;
-   if (YabauseExec() != 0)
-      return -1;
-   if (YabauseExec() != 0)
-      return -1;
-   if (YabauseExec() != 0)
-      return -1;
+	// create structure for neutral points
+	mSDLNeutralPoints = malloc( SDL_JoystickNumAxes( mSDLJoystick1 ) *sizeof( Sint16 ) );
+	// perform auto calibration
+	PERQtSDLScan( 0 );
+	// success
+	return 0;
+}
 
-   return 0;
+//////////////////////////////////////////////////////////////////////////////
+
+void PERQtSDLDeInit(void) {
+	// close joystick
+	if ( mSDLJoystick1 )
+	{
+		 if ( SDL_JoystickOpened( 0 ) )
+			SDL_JoystickClose( mSDLJoystick1 );
+		mSDLJoystick1 = 0;
+		free( mSDLNeutralPoints );
+		mSDLNeutralPoints = 0;
+	}
+	// close sdl joysticks
+	SDL_QuitSubSystem( SDL_INIT_JOYSTICK );
+	mCalibrationDone = 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void PERQtSDLNothing(void) {
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+// this may need be moved in the PerCore interface so all core can call it
+u32 hashAxisSDL( u8 a, s16 v )
+{
+	u32 r;
+	if ( !( v == -1 || v == 1 ) )
+		v = v < 0 ? -1 : 1;
+	r = v < 0 ? -v : v +1;
+	a % 2 ? r-- : r++;
+	r += 20 *( a +1 );
+	//r += 10 *( playerid +1 );
+	return r;
+}
+
+int isSDLJoysticksSame( Sint16* oav, Uint8* obv, Sint16* nav, Uint8* nbv, int na, int nb )
+{
+	int i;
+	for ( i = 0; i < na; i++ )
+		if ( oav[i] != nav[i] )
+			return -1;
+	for ( i = 0; i < nb; i++ )
+		if ( obv[i] != nbv[i] )
+			return -1;
+	return 0;
+}
+
+int PERQtSDLHandleEvents(void) {
+	// if available joy
+	if ( mSDLJoystick1 )
+	{
+		// check joystick
+		int i;
+		int na = SDL_JoystickNumAxes( mSDLJoystick1 );
+		int nb = SDL_JoystickNumButtons( mSDLJoystick1 );
+	
+		// remember last values before update joysticks values
+		Sint16* oav = malloc( na *sizeof( Sint16 ) );
+		for ( i = 0; i < na; i++ )
+			oav[i] = SDL_JoystickGetAxis( mSDLJoystick1, i );
+		Uint8* obv = malloc( nb *sizeof( Uint8 ) );
+		for ( i = 0; i < nb; i++ )
+			obv[i] = SDL_JoystickGetButton( mSDLJoystick1, i );
+		
+		// update joysticks states
+		SDL_JoystickUpdate();
+		
+		// get new values
+		Sint16* nav = malloc( na *sizeof( Sint16 ) );
+		for ( i = 0; i < na; i++ )
+			nav[i] = SDL_JoystickGetAxis( mSDLJoystick1, i );
+		Uint8* nbv = malloc( nb *sizeof( Uint8 ) );
+		for ( i = 0; i < nb; i++ )
+			nbv[i] = SDL_JoystickGetButton( mSDLJoystick1, i );
+		
+		// if differents update states
+		if ( isSDLJoysticksSame( oav, obv, nav, nbv, na, nb ) == -1 )
+		{
+			// check axis
+			for ( i = 0; i < na; i++ )
+			{
+				Sint16 cur = nav[i];
+				Sint16 cen = mSDLNeutralPoints[i];
+				if ( cur == cen )
+					PerKeyUp( hashAxisSDL( i, oav[i] ) );
+				else
+					PerKeyDown( hashAxisSDL( i, cur ) );
+			}
+			// check buttons
+			for ( i = 0; i < nb; i++ )
+			{
+				if ( nbv[i] )
+					PerKeyDown( i +1 );
+				else
+					PerKeyUp( i +1 );
+			}
+		}
+		// free values
+		free( oav );
+		free( obv );
+		free( nav );
+		free( nbv );
+	}
+	
+	if (YabauseExec() != 0)
+		return -1;
+	if (YabauseExec() != 0)
+		return -1;
+	if (YabauseExec() != 0)
+		return -1;
+	if (YabauseExec() != 0)
+		return -1;
+	if (YabauseExec() != 0)
+		return -1;
+	if (YabauseExec() != 0)
+		return -1;
+	if (YabauseExec() != 0)
+		return -1;
+	if (YabauseExec() != 0)
+		return -1;
+	return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 
 #ifdef USENEWPERINTERFACE
-PortData_struct *PERSDLJoyGetPerDataP1(void)
+PortData_struct *PERQtSDLGetPerDataP1(void)
 {
    port1.data[0] = 0xF1;
    port1.data[1] = 0x02;
@@ -180,7 +233,7 @@ PortData_struct *PERSDLJoyGetPerDataP1(void)
 
 //////////////////////////////////////////////////////////////////////////////
 
-PortData_struct *PERSDLJoyGetPerDataP2(void)
+PortData_struct *PERQtSDLGetPerDataP2(void)
 {
    port2.data[0] = 0xF0;
    port2.size = 1;
@@ -190,43 +243,62 @@ PortData_struct *PERSDLJoyGetPerDataP2(void)
 
 //////////////////////////////////////////////////////////////////////////////
 
-PerInfo_struct *PERSDLJoyGetList(void)
+PerInfo_struct *PERQtSDLGetList(void)
 {
    // Returns a list of peripherals available along with information on each
    // peripheral
+	return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 
-u32 PERSDLJoyScan(const char * name) {
-	SDL_Event event;
-	persdljoykey_struct k;
-
-	k.pack = 0;
-
-	SDL_PollEvent(&event);
-	k.data.type = event.type;
-	switch(event.type) {
-		case SDL_JOYAXISMOTION:
-			k.data.key = event.jaxis.axis;
-			k.data.value = event.jaxis.value;
-			break;
-		case SDL_JOYBUTTONDOWN:
-			k.data.key = event.jbutton.button;
-			break;
-		default:
-			k.pack = 0;
+u32 PERQtSDLScan( const char* n ) {
+	// if no available joy
+	if ( !mSDLJoystick1 )
+		return 0;
+	// check joystick
+	int i;
+	u32 k = 0;
+	// update joysticks states
+	SDL_JoystickUpdate();
+	// check axis
+	for ( i = 0; i < SDL_JoystickNumAxes( mSDLJoystick1 ); i++ )
+	{
+		// if joy is calibrate
+		if ( mCalibrationDone != 0 )
+		{
+			Sint16 cur = SDL_JoystickGetAxis( mSDLJoystick1, i );
+			Sint16 cen = mSDLNeutralPoints[i];
+			if ( cur != cen )
+			{
+				k = hashAxisSDL( i, cur );
+				break;
+			}
+		}
+		// calibrate axis
+		else
+			mSDLNeutralPoints[i] = SDL_JoystickGetAxis( mSDLJoystick1, i );
 	}
-
-	//PERSDLJoySetKey(k.pack, name);
-	PerSetKey(k.pack, name);
-	return k.pack;
+	// remember that axis are calibrate
+	mCalibrationDone = -1;
+	// check buttons
+	if ( k == 0 )
+	{
+		for ( i = 0; i < SDL_JoystickNumButtons( mSDLJoystick1 ); i++ )
+		{
+			if ( SDL_JoystickGetButton( mSDLJoystick1, i ) )
+			{
+				k = i +1;
+				break;
+			}
+		}
+	}
+	if ( k != 0 )
+		PerSetKey( k, n );
+	return k;
 }
 
-void PERSDLJoyFlush(void) {
-	SDL_Event event;
-
-	while(SDL_PollEvent(&event));
+void PERQtSDLFlush(void) {
 }
 
 #endif
