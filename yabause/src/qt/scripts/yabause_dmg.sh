d9bb1e5731e4b259684fd02195b1114250ce5f08
pasnox <pasnox@d7be59e3-070e-4b9a-85a1-4646befae34d>
2008-03-22 10:24:47 +0000

    No longer tweak the SDL framework, import it as simple copy like the native carbon port.
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1546 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/qt/scripts/yabause_dmg.sh
+++ yabause/src/qt/scripts/yabause_dmg.sh
@@ -5,7 +5,7 @@ QT_PLUGINGS=/usr/local/Trolltech/Qt-4.3.3/plugins
 YABAUSE_APP=../Yabause.app
 YABAUSE_FRAMEWORKS=$YABAUSE_APP/Contents/Frameworks
 YABAUSE=$YABAUSE_APP/Contents/MacOS/Yabause
-SDL_FRAMEWORK=/Developer/SDKs/MacOSX10.4u.sdk/Library/Frameworks/SDL.framework/Versions/A/SDL
+SDL_FRAMEWORK=/Developer/SDKs/MacOSX10.4u.sdk/Library/Frameworks/SDL.framework
 
 echo "Copying Qt 4 frameworks..."
 mkdir -p $YABAUSE_FRAMEWORKS
@@ -14,13 +14,13 @@ cp $QT_PATH_FRAMEWORKS/QtGui.framework/Versions/4/QtGui $YABAUSE_FRAMEWORKS
 cp $QT_PATH_FRAMEWORKS/QtOpenGL.framework/Versions/4/QtOpenGL $YABAUSE_FRAMEWORKS
 
 echo "Copying SDL framework..."
-cp $SDL_FRAMEWORK $YABAUSE_FRAMEWORKS
+rm -R $YABAUSE_FRAMEWORKS/SDL.framework
+cp -R $SDL_FRAMEWORK $YABAUSE_FRAMEWORKS
 
 echo "Relinking frameworks..."
 install_name_tool -id @executable_path/../Frameworks/QtCore $YABAUSE_FRAMEWORKS/QtCore
 install_name_tool -id @executable_path/../Frameworks/QtGui $YABAUSE_FRAMEWORKS/QtGui
 install_name_tool -id @executable_path/../Frameworks/QtOpenGL $YABAUSE_FRAMEWORKS/QtOpenGL
-install_name_tool -id @executable_path/../Frameworks/SDL $YABAUSE_FRAMEWORKS/SDL
 
 install_name_tool -change $QT_PATH_FRAMEWORKS/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore $YABAUSE_FRAMEWORKS/QtGui
 install_name_tool -change $QT_PATH_FRAMEWORKS/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore $YABAUSE_FRAMEWORKS/QtOpenGL
@@ -29,7 +29,6 @@ install_name_tool -change $QT_PATH_FRAMEWORKS/QtGui.framework/Versions/4/QtGui @
 install_name_tool -change $QT_PATH_FRAMEWORKS/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore $YABAUSE
 install_name_tool -change $QT_PATH_FRAMEWORKS/QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui $YABAUSE
 install_name_tool -change $QT_PATH_FRAMEWORKS/QtOpenGL.framework/Versions/4/QtOpenGL @executable_path/../Frameworks/QtOpenGL $YABAUSE
-install_name_tool -change $SDL_FRAMEWORK @executable_path/../Frameworks/SDL $YABAUSE
 
 echo "Creating temporary dmg..."
 hdiutil create -megabytes 30 tmp.dmg -layout NONE -fs HFS+ -volname Yabause -ov
