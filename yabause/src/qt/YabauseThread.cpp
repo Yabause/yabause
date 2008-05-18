f257bce3271fb266cbd9bf6fd7bb5b7e3ac88258
pasnox <pasnox@d7be59e3-070e-4b9a-85a1-4646befae34d>
2008-05-18 20:11:17 +0000

    Fix pad configuration with correct mappings
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1654 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/qt/YabauseThread.cpp
+++ yabause/src/qt/YabauseThread.cpp
@@ -1,22 +1,22 @@
 /*  Copyright 2005 Guillaume Duhamel
-    Copyright 2005-2006 Theo Berkau
+	Copyright 2005-2006 Theo Berkau
 	Copyright 2008 Filipe Azevedo <pasnox@gmail.com>
 
-    This file is part of Yabause.
+	This file is part of Yabause.
 
-    Yabause is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	Yabause is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    Yabause is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	Yabause is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Yabause; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with Yabause; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 #include "YabauseThread.h"
 #include "Settings.h"
@@ -24,6 +24,8 @@
 #include <QStringList>
 
 #include "../peripheral.h"
+
+extern const QStringList PadKeys;
 
 YabauseThread::YabauseThread( QObject* o )
 	: QObject( o )
@@ -150,18 +152,16 @@ void YabauseThread::reloadSettings()
 	emit requestFullscreen( s->value( "Video/Fullscreen", false ).toBool() );
 
 	{
-		PerPad_struct * padbits;
+		
+		PerPad_struct* padbits;
 		u8 i = 0;
 
 		PerPortReset();
-		padbits = PerPadAdd(&PORTDATA1);
-	
+		padbits = PerPadAdd( &PORTDATA1 );
+		
 		s->beginGroup( "Input/Keys" );
-		foreach ( const QString& ki, s->childKeys() )
-		{
-			PerSetKey( (u32)s->value( ki ).toString().toUInt(), i, padbits );
-			i++;
-		}
+		foreach ( const QString& pk, PadKeys )
+			PerSetKey( (u32)s->value( pk ).toString().toUInt(), PadKeys.indexOf( pk ), padbits );
 		s->endGroup();
 	}
 }
