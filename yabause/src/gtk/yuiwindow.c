19157b43290bb9100f66c42bfc65ab79c900b549
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2008-05-27 21:09:39 +0000

    Fixed the software core resolution in the gtk port after a restart.
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1682 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/gtk/yuiwindow.c
+++ yabause/src/gtk/yuiwindow.c
@@ -265,6 +265,7 @@ void yui_window_start(GtkWidget * w, YuiWindow * yui) {
 	  if (((int (*)(gpointer)) yui->init_func)(yui->init_data) == 0) {
 	    yui->state |= YUI_IS_INIT;
 	    gtk_action_set_sensitive(gtk_action_group_get_action(yui->action_group, "reset"), TRUE);
+            VIDCore->Resize(GTK_WIDGET(yui->area)->allocation.width, GTK_WIDGET(yui->area)->allocation.height, FALSE);
 	  }
 	}
 }
