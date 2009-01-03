68afddcae907f1b4f73600be1a94e524ae970710
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2009-01-03 13:45:27 +0000

    Started to document mini18n using doxygen
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1898 d7be59e3-070e-4b9a-85a1-4646befae34d

--- mini18n/src/mini18n.h
+++ mini18n/src/mini18n.h
@@ -20,6 +20,8 @@
 #ifndef MINI18N_H
 #define MINI18N_H
 
+/** @file */
+
 #define MINI18N_UTF16 1
 
 #ifndef _
@@ -30,11 +32,52 @@
 #define _16(source) (mini18n_with_conversion(source, MINI18N_UTF16))
 #endif
 
+/**
+ * @brief Select a translation matching the operating system configuration.
+ *
+ * @param folder the folder to search for translations.
+ * @returns 0 on success, -1 otherwise
+ */
 int mini18n_set_domain(const char * folder);
 int mini18n_set_locale(const char * locale);
 int mini18n_set_log(const char * filename);
+/**
+ * @brief Translates a string.
+ *
+ * @param source string to translate
+ * @returns The translated string on success, the source string otherwise. The returned value should not be freed or modified in any way.
+ */
 const char * mini18n(const char * source);
+/**
+ * @brief Translates and convert a string.
+ *
+ * The list of available conversion formats depends of the system.
+ * The converted value is stored so further calls to the function with the same source will return the same pointer.
+ *
+ * @param source String to translate.
+ * @param format The format to convert the string to.
+ */
 void * mini18n_with_conversion(const char * source, unsigned int format);
 void mini18n_close(void);
+
+/**
+ *
+ * @mainpage
+ *
+ * Mini18n is a translation library.
+ *
+ * \section using Using
+ *
+ * \subsection using-selecting Selecting translation
+ *
+ * Mini18n supports either automaticaly choosing a translation based on the current system settings or
+ * load a given translation file.
+ *
+ * To let Mini18n select the translation file, call mini18n_set_domain() with the base directory
+ * of translation files.
+ *
+ * \section extending Extending
+ *
+ */
 
 #endif
