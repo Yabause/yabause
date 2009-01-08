3b0abc68200b171c4bedace33e6cd69f9fe6877c
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2009-01-08 21:10:01 +0000

    Updated the FSF address to the current one. Maybe my larger commit ever... sorry for the noise :)
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@1911 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/dreamcast/cd.s
+++ yabause/src/dreamcast/cd.s
@@ -14,7 +14,7 @@
 !
 !   You should have received a copy of the GNU General Public License
 !   along with Yabause; if not, write to the Free Software
-!   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+!   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
 !   Ok, so this is the start of me assemblerizing different parts of the core
 !   of the Dreamcast port of Yabause. I picked the CD core since its one of the
