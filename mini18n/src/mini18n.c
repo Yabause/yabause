137613762c4de5113a543ee555fd6b3fbeafc9b7
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2009-10-18 13:27:04 +0000

    Fixed a stupid bug in mini18n, that fixes: https://bugs.launchpad.net/ubuntu/+source/yabause/+bug/450821
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@2409 d7be59e3-070e-4b9a-85a1-4646befae34d

--- mini18n/src/mini18n.c
+++ mini18n/src/mini18n.c
@@ -74,7 +74,7 @@ static char pathsep = '/';
 static void mini18n_pv_get_locale(char ** lang, char ** country) {
 	char * tmp;
 
-	*country = NULL;
+	*country = "";
 	*lang = NULL;
 	tmp = getenv("LANG");
 	if (tmp == NULL) return;
