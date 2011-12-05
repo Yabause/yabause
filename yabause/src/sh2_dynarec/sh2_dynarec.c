ea249537cb87bc9b19c319ee36e962bf147fc36c
ari64 <ari64@d7be59e3-070e-4b9a-85a1-4646befae34d>
2011-12-05 22:47:58 +0000

    SH2 dynarec: If both registers of a MAC (multiply-accumulate) instruction are the same, the register is incremented twice.
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@2744 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/sh2_dynarec/sh2_dynarec.c
+++ yabause/src/sh2_dynarec/sh2_dynarec.c
@@ -2796,6 +2796,18 @@ void complex_assemble(int i,struct regstat *i_regs)
   }
   if(opcode[i]==0&&opcode2[i]==15) { // MAC.L
     load_regs(i_regs->regmap_entry,i_regs->regmap,MACL,MACH,MACH);
+    // If both registers are the same, the register is incremented twice.
+    // Pre-increment one of the function arguments.
+    #if defined(__i386__) || defined(__x86_64__)
+    if(rs1[i]==rs2[i]) {emit_mov(EDI,ESI);emit_addimm(EDI,4,EDI);}
+    #else
+    #if defined(__arm__)
+    if(rs1[i]==rs2[i]) {emit_mov(6,5);emit_addimm(6,4,6);}
+    #else
+    // FIXME
+    assert(0);
+    #endif
+    #endif
 /* DEBUG
   //if(i_regmap[HOST_CCREG]!=CCREG) {
     emit_loadreg(CCREG,ECX);
@@ -2810,6 +2822,18 @@ void complex_assemble(int i,struct regstat *i_regs)
   }
   if(opcode[i]==4&&opcode2[i]==15) { // MAC.W
     load_regs(i_regs->regmap_entry,i_regs->regmap,MACL,MACH,MACH);
+    // If both registers are the same, the register is incremented twice.
+    // Pre-increment one of the function arguments.
+    #if defined(__i386__) || defined(__x86_64__)
+    if(rs1[i]==rs2[i]) {emit_mov(EDI,ESI);emit_addimm(EDI,2,EDI);}
+    #else
+    #if defined(__arm__)
+    if(rs1[i]==rs2[i]) {emit_mov(6,5);emit_addimm(6,2,6);}
+    #else
+    // FIXME
+    assert(0);
+    #endif
+    #endif
 /* DEBUG
   //if(i_regmap[HOST_CCREG]!=CCREG) {
     emit_loadreg(CCREG,ECX);
