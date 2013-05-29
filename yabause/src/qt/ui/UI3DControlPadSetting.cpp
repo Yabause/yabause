c7f4d582239c336d8eaa51a67d243f04bfbf8b22
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2013-05-29 19:36:09 +0000

    Fixed case in #include
    git-svn-id: https://yabause.svn.sourceforge.net/svnroot/yabause/trunk@3127 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/qt/ui/UI3DControlPadSetting.cpp
+++ yabause/src/qt/ui/UI3DControlPadSetting.cpp
@@ -16,7 +16,7 @@
         along with Yabause; if not, write to the Free Software
         Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
-#include "UI3dControlPadSetting.h"
+#include "UI3DControlPadSetting.h"
 #include "UIPortManager.h"
 #include "../Settings.h"
 
