8b437355ce6e66e7d1bda2dcc84a2ac0c6508bb7
yabause <yabause@d7be59e3-070e-4b9a-85a1-4646befae34d>
2013-12-13 13:14:43 +0000

    Fixed shortcut interface translations
    git-svn-id: svn+ssh://svn.code.sf.net/p/yabause/code/trunk@3214 d7be59e3-070e-4b9a-85a1-4646befae34d

--- yabause/src/qt/ui/UISettings.cpp
+++ yabause/src/qt/ui/UISettings.cpp
@@ -349,7 +349,7 @@ void UISettings::loadShortcuts()
 	twShortcuts->setRowCount(actionsList.count());
 	foreach ( QAction* action, actionsList )
 	{
-		QTableWidgetItem *tblItem = new QTableWidgetItem(action->text());
+		QTableWidgetItem *tblItem = new QTableWidgetItem(QString::fromUtf8(_(action->text().toUtf8())));
 		tblItem->setFlags(tblItem->flags() ^ Qt::ItemIsEditable);
 		twShortcuts->setItem(row, 0, tblItem);
 		tblItem = new QTableWidgetItem(action->shortcut().toString());
