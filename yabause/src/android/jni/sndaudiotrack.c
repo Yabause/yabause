3778f5ddf9ecf0c4e4cd9751aa9191a96841f537
Guillaume <guillaume.duhamel@gmail.com>
2015-04-11 20:07:04 +0200

    Fixed call to audiotrack write function.
    This fix a crash when using sound on Android with ART runtime.

--- yabause/src/android/jni/sndaudiotrack.c
+++ yabause/src/android/jni/sndaudiotrack.c
@@ -177,12 +177,14 @@ static void SNDAudioTrackUpdateAudio(u32 *leftchanbuffer, u32 *rightchanbuffer, 
    if (soundoffset > mbufferSizeInBytes) {
       if (! muted) {
          JNIEnv * env;
+         jbyteArray array;
+
          if ((*yvm)->GetEnv(yvm, (void**) &env, JNI_VERSION_1_6) != JNI_OK)
              return;
 
-         jshortArray array = (*env)->NewShortArray(env, soundoffset);
+         array = (*env)->NewByteArray(env, soundoffset);
          if(array) {
-            (*env)->SetShortArrayRegion(env, array, 0, soundoffset, stereodata16);
+            (*env)->SetByteArrayRegion(env, array, 0, soundoffset, (u8 *) stereodata16);
          }
 
          (*env)->CallNonvirtualIntMethod(env, gtrack, cAudioTrack, mWrite, array, 0, soundoffset);
