04d07af80c5d7e66386512d60ea03292c6567ed3
devmiyax <smiyaxdev@gmail.com>
2015-07-13 03:14:21 +0900

    build for Android 4.4 and Windows QT

--- yabause/src/thr-windows.c
+++ yabause/src/thr-windows.c
@@ -132,4 +132,125 @@ void YabThreadWake(unsigned int id)
    SetEvent(thread_handle[id].cond);
 }
 
+typedef struct YabEventQueue_win32
+{
+	int *buffer;
+	int capacity;
+	int size;
+	int in;
+	int out;
+//	pthread_mutex_t mutex;
+//	pthread_cond_t cond_full;
+//	pthread_cond_t cond_empty;
+} YabEventQueue_win32;
+
+
+YabEventQueue * YabThreadCreateQueue(int qsize){
+	YabEventQueue_win32 * p = (YabEventQueue_win32*)malloc(sizeof(YabEventQueue_win32));
+	p->buffer = (int*)malloc(sizeof(int)* qsize);
+	p->capacity = qsize;
+	p->size = 0;
+	p->in = 0;
+	p->out = 0;
+	//pthread_mutex_init(&p->mutex, NULL);
+	//pthread_cond_init(&p->cond_full, NULL);
+	//pthread_cond_init(&p->cond_empty, NULL);
+
+	return (YabEventQueue *)p;
+}
+
+void YabThreadDestoryQueue(YabEventQueue * queue_t){
+#if 1
+	// Todo: implement Windows version
+#else
+	//pthread_mutex_t mutex;
+	YabEventQueue_win32 * queue = (YabEventQueue_pthread*)queue_t;
+	mutex = queue->mutex;
+	pthread_mutex_lock(&mutex);
+	while (queue->size == queue->capacity)
+		pthread_cond_wait(&(queue->cond_full), &(queue->mutex));
+	free(queue->buffer);
+	free(queue);
+	pthread_mutex_unlock(&mutex);
+#endif
+}
+
+
+
+void YabAddEventQueue(YabEventQueue * queue_t, int evcode){
+#if 1
+	// Todo: implement Windows version
+#else
+	YabEventQueue_pthread * queue = (YabEventQueue_pthread*)queue_t;
+	pthread_mutex_lock(&(queue->mutex));
+	while (queue->size == queue->capacity)
+		pthread_cond_wait(&(queue->cond_full), &(queue->mutex));
+	queue->buffer[queue->in] = evcode;
+	++queue->size;
+	++queue->in;
+	queue->in %= queue->capacity;
+	pthread_mutex_unlock(&(queue->mutex));
+	pthread_cond_broadcast(&(queue->cond_empty));
+#endif
+}
+
+
+int YabWaitEventQueue(YabEventQueue * queue_t){
+#if 1
+	// Todo: implement Windows version
+	return 0;
+#else
+	int value;
+	YabEventQueue_pthread * queue = (YabEventQueue_pthread*)queue_t;
+	pthread_mutex_lock(&(queue->mutex));
+	while (queue->size == 0)
+		pthread_cond_wait(&(queue->cond_empty), &(queue->mutex));
+	value = queue->buffer[queue->out];
+	--queue->size;
+	++queue->out;
+	queue->out %= queue->capacity;
+	pthread_mutex_unlock(&(queue->mutex));
+	pthread_cond_broadcast(&(queue->cond_full));
+	return value;
+#endif
+}
+
+
+typedef struct YabMutex_win32
+{
+//	pthread_mutex_t mutex;
+	HANDLE mutex;
+} YabMutex_win32;
+
+void YabThreadLock(YabMutex * mtx){
+#if 1
+	// Todo: implement Windows version
+#else
+	YabMutex_pthread * pmtx;
+	pmtx = (YabMutex_pthread *)mtx;
+	pthread_mutex_lock(&pmtx->mutex);
+#endif 
+}
+
+void YabThreadUnLock(YabMutex * mtx){
+#if 1
+	// Todo: implement Windows version
+#else
+	YabMutex_pthread * pmtx;
+	pmtx = (YabMutex_pthread *)mtx;
+	pthread_mutex_unlock(&pmtx->mutex);
+#endif
+}
+
+YabMutex * YabThreadCreateMutex(){
+#if 1
+	// Todo: implement Windows version
+	return (YabMutex *)0x000000;
+#else
+	YabMutex_pthread * mtx = (YabMutex_pthread *)malloc(sizeof(YabMutex_pthread));
+	pthread_mutex_init(&mtx->mutex, NULL);
+	return (YabMutex *)mtx;
+#endif
+}
+
 //////////////////////////////////////////////////////////////////////////////
