af4c8de2c221d02d39114392c1c7ed6b56933dbb
devmiyax <smiyaxdev@gmail.com>
2015-08-22 16:42:33 +0900

    VDP2 FrameBuffer Add Color operation

--- yabause/src/ygles.c
+++ yabause/src/ygles.c
@@ -2099,8 +2099,11 @@ void YglRenderFrameBuffer( int from , int to ) {
    offsetcol[2] = vdp1cob / 255.0f;
    offsetcol[3] = 0.0f;
 
-   if (Vdp2Regs->LNCLEN & 0x20){
-     Ygl_uniformVDP2DrawFramebuffer_linecolor(&_Ygl->renderfb, (float)(from) / 10.0f, (float)(to) / 10.0f, offsetcol);
+   if ( (Vdp2Regs->CCCTL & 0x140) == 0x140 ){
+		// Sprite Add Color
+	   Ygl_uniformVDP2DrawFramebuffer_addcolor(&_Ygl->renderfb, (float)(from) / 10.0f, (float)(to) / 10.0f, offsetcol);
+   }else if (Vdp2Regs->LNCLEN & 0x20){
+		Ygl_uniformVDP2DrawFramebuffer_linecolor(&_Ygl->renderfb, (float)(from) / 10.0f, (float)(to) / 10.0f, offsetcol);
    }
    else{
      Ygl_uniformVDP2DrawFramebuffer(&_Ygl->renderfb, (float)(from) / 10.0f, (float)(to) / 10.0f, offsetcol);
