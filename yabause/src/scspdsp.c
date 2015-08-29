33c60db2a685621738fb26e97297767be91d9143
d356 <d356@users.noreply.github.com>
2015-08-29 12:26:08 -0400

    Fix SCSP DSP instruction decoding and add an assembler

--- yabause/src/scspdsp.c
+++ yabause/src/scspdsp.c
@@ -20,6 +20,172 @@
 #include "scsp.h"
 #include "scspdsp.h"
 
+int ScspDspAssembleGetValue(char* instruction)
+{
+   char temp[512] = { 0 };
+   int value = 0;
+   sscanf(instruction, "%s %d", temp, &value);
+   return value;
+}
+
+u64 ScspDspAssembleLine(char* line)
+{
+   union ScspDspInstruction instruction = { 0 };
+
+   char* temp = NULL;
+
+   if (temp = strstr(line, "tra"))
+   {
+      instruction.part.tra = ScspDspAssembleGetValue(temp);
+   }
+
+   if (strstr(line, "twt"))
+   {
+      instruction.part.twt = 1;
+   }
+
+   if (temp = strstr(line, "twa"))
+   {
+      instruction.part.twa = ScspDspAssembleGetValue(temp);
+   }
+
+   if (strstr(line, "xsel"))
+   {
+      instruction.part.xsel = 1;
+   }
+
+   if (temp = strstr(line, "ysel"))
+   {
+      instruction.part.ysel = ScspDspAssembleGetValue(temp);
+   }
+
+   if (temp = strstr(line, "ira"))
+   {
+      instruction.part.ira = ScspDspAssembleGetValue(temp);
+   }
+
+   if (strstr(line, "iwt"))
+   {
+      instruction.part.iwt = 1;
+   }
+
+   if (temp = strstr(line, "iwa"))
+   {
+      instruction.part.iwa = ScspDspAssembleGetValue(temp);
+   }
+
+   if (strstr(line, "table"))
+   {
+      instruction.part.table = 1;
+   }
+
+   if (strstr(line, "mwt"))
+   {
+      instruction.part.mwt = 1;
+   }
+
+   if (strstr(line, "mrd"))
+   {
+      instruction.part.mrd = 1;
+   }
+
+   if (strstr(line, "ewt"))
+   {
+      instruction.part.ewt = 1;
+   }
+
+   if (temp = strstr(line, "ewa"))
+   {
+      instruction.part.ewa = ScspDspAssembleGetValue(temp);
+   }
+
+   if (strstr(line, "adrl"))
+   {
+      instruction.part.adrl = 1;
+   }
+
+   if (strstr(line, "frcl"))
+   {
+      instruction.part.frcl = 1;
+   }
+
+   if (temp = strstr(line, "shift"))
+   {
+      instruction.part.shift = ScspDspAssembleGetValue(temp);
+   }
+
+   if (strstr(line, "yrl"))
+   {
+      instruction.part.yrl = 1;
+   }
+
+   if (strstr(line, "negb"))
+   {
+      instruction.part.negb = 1;
+   }
+
+   if (strstr(line, "zero"))
+   {
+      instruction.part.zero = 1;
+   }
+
+   if (strstr(line, "bsel"))
+   {
+      instruction.part.bsel = 1;
+   }
+
+   if (strstr(line, "nofl"))
+   {
+      instruction.part.nofl = 1;
+   }
+
+   if (temp = strstr(line, "coef"))
+   {
+      instruction.part.coef = ScspDspAssembleGetValue(temp);
+   }
+
+   if (temp = strstr(line, "masa"))
+   {
+      instruction.part.masa = ScspDspAssembleGetValue(temp);
+   }
+
+   if (strstr(line, "adreb"))
+   {
+      instruction.part.adreb = 1;
+   }
+
+   if (strstr(line, "nxadr"))
+   {
+      instruction.part.adreb = 1;
+   }
+
+   if (strstr(line, "nop"))
+   {
+      instruction.all = 0;
+   }
+
+   return instruction.all;
+}
+
+void ScspDspAssembleFromFile(char * filename, u64* output)
+{
+   int i;
+   char line[1024] = { 0 };
+
+   FILE * fp = fopen(filename, "r");
+
+   if (!fp)
+   {
+      return;
+   }
+
+   for (i = 0; i < 128; i++)
+   {
+      fgets(line, sizeof(line), fp);
+      output[i] = ScspDspAssembleLine(line);
+   }
+}
+
 void ScspDspDisasm(u8 addr, char *outstring)
 {
    union ScspDspInstruction instruction;
@@ -203,4 +369,24 @@ void ScspDspDisasm(u8 addr, char *outstring)
       sprintf(outstring, "unknown3 %d", (int)(instruction.part.unknown3 & 3));
       outstring += strlen(outstring);
    }
+}
+
+void ScspDspDisassembleToFile(char * filename)
+{
+   int i;
+   FILE * fp = fopen(filename,"w");
+
+   if (!fp)
+   {
+      return;
+   }
+
+   for (i = 0; i < 128; i++)
+   {
+      char output[1024] = { 0 };
+      ScspDspDisasm(i, output);
+      fprintf(fp, "%s\n", output);
+   }
+
+   fclose(fp);
 }
