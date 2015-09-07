46bd80bd8f076f4e60cf86d18e8a5f259e962f07
devmiyax <smiyaxdev@gmail.com>
2015-09-08 03:20:18 +0900

    Texture Barrier on Desktop OpenGL

--- yabause/src/yglshaderes.c
+++ yabause/src/yglshaderes.c
@@ -312,23 +312,23 @@ const GLchar * pYglprg_vdp1_gouraudshading_v[] = {Yglprg_vdp1_gouraudshading_v, 
 
 const GLchar Yglprg_vdp1_gouraudshading_f[] =
 #if defined(_OGLES3_)
-      "#version 300 es \n"
+"#version 300 es \n"
 #else
-      "#version 330 \n"
+"#version 330 \n"
 #endif
-      "precision highp float;                                                 \n"
-      "uniform sampler2D u_sprite;                                              \n"
-      "in vec4 v_texcoord;                                                 \n"
-      "in vec4 v_vtxcolor;                                                 \n"
-      "out vec4 fragColor;            \n"
-      "void main() {                                                            \n"
-      "  vec2 addr = v_texcoord.st;                                             \n"
-      "  addr.s = addr.s / (v_texcoord.q);                                      \n"
-      "  addr.t = addr.t / (v_texcoord.q);                                      \n"
-      "  vec4 spriteColor = texture(u_sprite,addr);                           \n"
-      "  if( spriteColor.a == 0.0 ) discard;                                      \n"
-      "  fragColor   = spriteColor;                                          \n"
-      "  fragColor  = clamp(spriteColor+v_vtxcolor,vec4(0.0),vec4(1.0));     \n"
+"precision highp float;                                                 \n"
+"uniform sampler2D u_sprite;                                              \n"
+"in vec4 v_texcoord;                                                 \n"
+"in vec4 v_vtxcolor;                                                 \n"
+"out vec4 fragColor;            \n"
+"void main() {                                                            \n"
+"  vec2 addr = v_texcoord.st;                                             \n"
+"  addr.s = addr.s / (v_texcoord.q);                                      \n"
+"  addr.t = addr.t / (v_texcoord.q);                                      \n"
+"  vec4 spriteColor = texture(u_sprite,addr);                           \n"
+"  if( spriteColor.a == 0.0 ) discard;                                      \n"
+"  fragColor   = spriteColor;                                          \n"
+"  fragColor  = clamp(spriteColor+v_vtxcolor,vec4(0.0),vec4(1.0));     \n"
       "  fragColor.a = spriteColor.a;                                        \n"
       "}\n";
 const GLchar * pYglprg_vdp1_gouraudshading_f[] = {Yglprg_vdp1_gouraudshading_f, NULL};
@@ -437,6 +437,9 @@ int Ygl_uniformGlowShadingHalfTrans(void * p )
    glUniform1i(id_fbowidth, GlWidth);
    glUniform1i(id_fboheight, GlHeight);
    glActiveTexture(GL_TEXTURE0);
+#if !defined(_OGLES3_)
+   glTextureBarrierNV();
+#endif
    return 0;
 }
 
@@ -525,6 +528,9 @@ int Ygl_uniformHalfTrans(void * p )
    glUniform1i(id_hf_fbowidth, GlWidth);
    glUniform1i(id_hf_fboheight, GlHeight);
    glActiveTexture(GL_TEXTURE0);
+#if !defined(_OGLES3_)
+   glTextureBarrierNV();
+#endif
    return 0;
 }
 
