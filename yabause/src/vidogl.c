b16213944eba414ce0ec47c91fe1c123f8b0cb67
devMiyax <smiyaxdev@gmail.com>
2015-12-26 07:28:21 +0900

    fixed #107 [Samurai Spirits IV (J)] Vertical thin line on the character

--- yabause/src/vidogl.c
+++ yabause/src/vidogl.c
@@ -3550,8 +3550,10 @@ void VIDOGLVdp1ScaledSpriteDraw(u8 * ram, Vdp1 * regs, u8* back_framebuffer)
    switch ((cmd.CMDCTRL & 0xF00) >> 8)
    {
       case 0x0: // Only two coordinates
-         rw = cmd.CMDXC - x + Vdp1Regs->localX + 1;
-         rh = cmd.CMDYC - y + Vdp1Regs->localY + 1;
+         rw = cmd.CMDXC - x + Vdp1Regs->localX;
+         rh = cmd.CMDYC - y + Vdp1Regs->localY;
+		 if (cmd.CMDXA < cmd.CMDXC) rw += 1; else rw -= 1;
+		 if (cmd.CMDYA < cmd.CMDYC) rh += 1; else rh -= 1;
          break;
       case 0x5: // Upper-left
          rw = cmd.CMDXB + 1;
