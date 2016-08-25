b9afbbee3859bb1012d3c1b0a3786c41fdc357c6
Guillaume <guillaume.duhamel@gmail.com>
2016-08-25 21:56:26 +0200

    Improve Android build
    With this change, the Android port can be compiled by only
    setting ANDROID_HOME and ANDROID_NDK_HOME.
    
    It's using d356 script for travis.

--- yabause/src/android/android.cmake
+++ yabause/src/android/android.cmake
@@ -1,9 +1,22 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
-SET(CMAKE_C_COMPILER   arm-linux-androideabi-gcc)
-SET(CMAKE_CXX_COMPILER arm-linux-androideabi-g++)
-SET(CMAKE_ASM-ATT_COMPILER arm-linux-androideabi-as)
+if (NOT DEFINED ENV{ANDROID_NDK_HOME})
+	message(FATAL_ERROR "Please set ANDROID_NDK_HOME environment variable")
+endif()
+
+set(TOOLCHAIN "$ENV{ANDROID_NDK_HOME}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/")
+
+SET(CMAKE_C_COMPILER ${TOOLCHAIN}/arm-linux-androideabi-gcc)
+SET(CMAKE_CXX_COMPILER ${TOOLCHAIN}/arm-linux-androideabi-g++)
+SET(CMAKE_ASM-ATT_COMPILER ${TOOLCHAIN}/arm-linux-androideabi-as)
+
+set(SYSROOT "$ENV{ANDROID_NDK_HOME}/platforms/android-24/arch-arm/usr")
+
+set(CMAKE_C_FLAGS "--sysroot=${SYSROOT}" CACHE STRING "GCC flags" FORCE)
+set(CMAKE_CXX_FLAGS "--sysroot=${SYSROOT}" CACHE STRING "G++ flags" FORCE)
+
+set(CMAKE_FIND_ROOT_PATH ${SYSROOT})
 
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
