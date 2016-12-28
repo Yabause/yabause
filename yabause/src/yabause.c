53f9d5976bae4ce9b8b342ac6aa1e52f7ca27950
devMiyax <smiyaxdev@gmail.com>
2016-12-28 20:12:54 +0900

    refs #288 [GUNGRIFFON] Black screen when game start

--- yabause/src/yabause.c
+++ yabause/src/yabause.c
@@ -1146,6 +1146,8 @@ int YabauseQuickLoadGame(void)
 
       // Now setup SH2 registers to start executing at ip code
       SH2GetRegisters(MSH2, &MSH2->regs);
+      MSH2->onchip.VCRC = 0x64 << 8;
+      MSH2->onchip.IPRB = 0x0F00;
       MSH2->regs.PC = 0x06002E00;
       MSH2->regs.R[15] = Cs2GetMasterStackAdress();
       SH2SetRegisters(MSH2, &MSH2->regs);
