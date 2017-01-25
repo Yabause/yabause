c494735ac38043d75495e8104c1e4ed66f9d9d0b
Francois CARON <francois.caron.perso@gmail.com>
2017-01-25 10:10:14 +0100

    Use dynarec when present

--- yabause/src/sdl/main.c
+++ yabause/src/sdl/main.c
@@ -204,7 +204,7 @@ void YuiSwapBuffers(void) {
 void YuiInit() {
 	yinit.m68kcoretype = M68KCORE_MUSASHI;
 	yinit.percoretype = PERCORE_LINUXJOY;
-	yinit.sh2coretype = SH2CORE_DEFAULT;
+	yinit.sh2coretype = 2;
 #ifdef FORCE_CORE_SOFT
   yinit.vidcoretype = VIDCORE_SOFT;
 #else
