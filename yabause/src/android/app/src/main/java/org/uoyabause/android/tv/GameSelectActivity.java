c5e8a0deb65ef8a506856a83b4646c3ca435fdc6
devMiyax <smiyaxdev@gmail.com>
2017-02-18 15:06:53 +0900

    Update Libraries to Android 7.0(api level 25)

--- yabause/src/android/app/src/main/java/org/uoyabause/android/tv/GameSelectActivity.java
+++ yabause/src/android/app/src/main/java/org/uoyabause/android/tv/GameSelectActivity.java
@@ -14,14 +14,19 @@
 
 package org.uoyabause.android.tv;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.InputDevice;
 import android.view.KeyEvent;
+import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
