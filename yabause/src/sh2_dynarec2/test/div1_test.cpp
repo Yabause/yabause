b6fddbd8be491fd228bba1239dcdaaf987ca1b3e
devMiyax <smiyaxdev@gmail.com>
2017-06-03 09:43:49 +0900

    FIX DIV1

--- yabause/src/sh2_dynarec2/test/div1_test.cpp
+++ yabause/src/sh2_dynarec2/test/div1_test.cpp
@@ -51,5 +51,48 @@ TEST_F(Div1Test, normal) {
   EXPECT_EQ( 0x00000001, pctx_->GET_SR() );
 }
 
+//060133C8: DIV1(i) 3304 R[0]:000000D9,R[3]:FFFFFFD1,SR:00000100
+//060133C8: DIV1(O) m:000000D9,n:0000007B,SR:00000001
+
+TEST_F(Div1Test, normal02) {
+
+  pctx_->GetGenRegPtr()[0]=0x000000D9; // m
+  pctx_->GetGenRegPtr()[3]=0xFFFFFFD1; // n
+  pctx_->SET_SR(0x0000100);
+
+  // subc r1,r2
+  memSetWord( 0x06000000, 0x3304 );
+  memSetWord( 0x06000002, 0x000b );  // rts
+  memSetWord( 0x06000004, 0x0009 );  // nop
+
+  pctx_->SET_PC( 0x06000000 );
+  pctx_->Execute();
+
+  EXPECT_EQ( 0x000000D9, pctx_->GetGenRegPtr()[0] );
+  EXPECT_EQ( 0x0000007B, pctx_->GetGenRegPtr()[3] );
+  EXPECT_EQ( 0x00000001, pctx_->GET_SR() );
+}
+
+//06013340: DIV1i 3304 R[0]:00000008,R[3]:FFFFFFF8,SR:000001E0
+//06013340: DIV1o m:00000008,n:FFFFFFF8,SR:000001E0
+
+TEST_F(Div1Test, normal03) {
+
+  pctx_->GetGenRegPtr()[0]=0x00000008; // m
+  pctx_->GetGenRegPtr()[3]=0xFFFFFFF8; // n
+  pctx_->SET_SR(0x000001E0);
+
+  // subc r1,r2
+  memSetWord( 0x06000000, 0x3304 );
+  memSetWord( 0x06000002, 0x000b );  // rts
+  memSetWord( 0x06000004, 0x0009 );  // nop
+
+  pctx_->SET_PC( 0x06000000 );
+  pctx_->Execute();
+
+  EXPECT_EQ( 0x00000008, pctx_->GetGenRegPtr()[0] );
+  EXPECT_EQ( 0xFFFFFFF8, pctx_->GetGenRegPtr()[3] );
+  EXPECT_EQ( 0x000001E0, pctx_->GET_SR() );
+}
 
 }  // namespacegPtr
