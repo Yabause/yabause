bdc3c40c18b7d0f349704b0f48d9a4af5e34708d
Israel Jacquez <mrkotfw@gmail.com>
2017-07-05 23:17:33 -0700

    Enumerate each command table in QT VDP1 debug window

--- yabause/src/vdp1.h
+++ yabause/src/vdp1.h
@@ -181,7 +181,7 @@ void FASTCALL Vdp1ReadCommand(vdp1cmd_struct *cmd, u32 addr, u8* ram);
 int Vdp1SaveState(FILE *fp);
 int Vdp1LoadState(FILE *fp, int version, int size);
 
-char *Vdp1DebugGetCommandNumberName(u32 number);
+void Vdp1DebugGetCommandNumberName(u32 number, char *outstring);
 void Vdp1DebugCommand(u32 number, char *outstring);
 u32 *Vdp1DebugTexture(u32 number, int *w, int *h);
 void ToggleVDP1(void);
