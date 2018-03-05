487f4715f3043018d0fdc8574d558a7b3a8b7510
devMiyax <smiyaxdev@gmail.com>
2018-03-05 23:47:42 +0900

    fix regression RBG0 is slow

--- yabause/src/vidogl.c
+++ yabause/src/vidogl.c
@@ -28,7 +28,7 @@
 #define EPSILON (1e-10 )
 
 
-#include "vidogl.h"
+#include "vidogl.h" 
 #include "vidshared.h"
 #include "debug.h"
 #include "vdp2.h"
@@ -3065,6 +3065,8 @@ static void Vdp2DrawRotationSync() {
     }
   }
 }
+
+#define ceilf(a) ((a)+0.99999f)
 
 static INLINE int vdp2rGetKValue(vdp2rotationparameter_struct * parameter, int i) {
   float kval;
