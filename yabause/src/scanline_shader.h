1c6371ebdcbcae4da7b142d69f72dbcd695b8bf5
LeCare <francois.caron.perso@gmail.com>
2018-03-14 23:14:11 +0100

    Scanline less black - looks like interlaced screen

--- yabause/src/scanline_shader.h
+++ yabause/src/scanline_shader.h
@@ -2,6 +2,6 @@
 #define __SCANLINE_INCLUDE_H__
 static const GLchar Yglprg_blit_scanline_f[] =
 "    float alpha = cos(3.1415*lineNumber[1]/decim * (vTexCoord.y+1.0)/2.0);\n" //Analog ray of CRT looks like a square sine
-"    alpha = alpha*alpha;\n"
+"    alpha = 0.5 + (alpha*alpha)/2.0;\n"
 "    fragColor = vec4(fragColor.xyz*alpha, 1.0);\n";
 #endif
