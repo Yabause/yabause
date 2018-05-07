5496b1ffb4d17ecef455d44785b690dd948a41c9
LeCare <francois.caron.perso@gmail.com>
2018-05-07 23:26:08 +0200

    Remove compiltion warning

--- yabause/src/eeprom.h
+++ yabause/src/eeprom.h
@@ -7,6 +7,7 @@ extern void eeprom_set_clk(u8 state);
 extern void eeprom_set_di(u8 state);
 extern void eeprom_set_cs(u8 state);
 extern int eeprom_do_read();
+extern void eeprom_init(const char* filename);
 
 #endif
 
