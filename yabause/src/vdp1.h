83523c7b7027c4e4baca23c86add2072a9ef977d
LeCare <francois.caron.perso@gmail.com>
2018-08-29 15:39:32 +0200

    Fix compilation

--- yabause/src/vdp1.h
+++ yabause/src/vdp1.h
@@ -175,5 +175,10 @@ void Vdp1DebugCommand(u32 number, char *outstring);
 u32 *Vdp1DebugTexture(u32 number, int *w, int *h);
 void ToggleVDP1(void);
 
+void Vdp1HBlankIN(void);
+void Vdp1HBlankOUT(void);
+void Vdp1VBlankIN(void);
+void Vdp1VBlankOUT(void);
+
 
 #endif
