aea6d44dc104014e5defaf735474e65c5b1bd688
CARON Francois <francois.caron@renault.com>
2018-12-19 14:33:39 +0100

    Use a safer version of vsnprintf

--- yabause/src/qt/QtYabause.cpp
+++ yabause/src/qt/QtYabause.cpp
@@ -159,25 +159,11 @@ extern "C"
 {
 
 #ifdef WIN32
-#ifndef _vscprintf
-		/* For some reason, MSVC fails to honour this #ifndef. */
-		/* Hence function renamed to _vscprintf_so(). */
-		int _vscprintf_so(const char * format, va_list pargs) {
-			int retval;
-			va_list argcopy;
-			va_copy(argcopy, pargs);
-			retval = vsnprintf(NULL, 0, format, argcopy);
-			va_end(argcopy);
-			return retval;
-		}
-#endif // _vscprintf
 #ifndef vasprintf
 		int vasprintf(char **strp, const char *fmt, va_list ap) {
-			int len = _vscprintf_so(fmt, ap);
-			if (len == -1) return -1;
-			char *str = (char*)malloc((size_t)len + 1);
-			if (!str) return -1;
-			int r = vsnprintf(str, len + 1, fmt, ap); /* "secure" version of vsprintf */
+			int len = 512
+			char *str = (char*)malloc((size_t)len);
+			int r = vsnprintf_s(str, len, _TRUNCATE, fmt, ap); /* "secure" version of vsprintf */
 			if (r == -1) return free(str), -1;
 			*strp = str;
 			return r;
