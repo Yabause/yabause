523dc84bf2799611839ce9b7cbc42c52c62771e1
devMiyax <smiyaxdev@gmail.com>
2019-07-21 23:21:11 +0900

    Disable MSB Shadow temporally
    (cherry picked from commit 056b221091ab7c1bd8273c514175c078cb248431)

--- yabause/src/ygles.c
+++ yabause/src/ygles.c
@@ -3655,7 +3655,7 @@ void YglRender(void) {
   }
 
    if ((fixVdp2Regs->SDCTL & 0xFF) != 0 || _Ygl->msb_shadow_count_[_Ygl->readframe] != 0 ) {
-     YglRenderFrameBufferShadow();
+     //YglRenderFrameBufferShadow();
    }
 
 
