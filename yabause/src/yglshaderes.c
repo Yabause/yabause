5f49eb7db391d96181942cfd89751772f94cb414
LeCare <francois.caron.perso@gmail.com>
2019-09-16 13:54:48 +0200

    Fix MSB color calculation

--- yabause/src/yglshaderes.c
+++ yabause/src/yglshaderes.c
@@ -1490,7 +1490,7 @@ const GLchar Yglprg_vdp2_drawfb_cram_no_color_col_f[]    = " alpha = opaque; \n"
 const GLchar Yglprg_vdp2_drawfb_cram_less_color_col_f[]  = " if( depth > u_cctl ){ alpha = opaque; fbmode = 0;} \n ";
 const GLchar Yglprg_vdp2_drawfb_cram_equal_color_col_f[] = " if( depth != u_cctl ){ alpha = opaque; fbmode = 0;} \n ";
 const GLchar Yglprg_vdp2_drawfb_cram_more_color_col_f[]  = " if( depth < u_cctl ){ alpha = opaque; fbmode = 0;} \n ";
-const GLchar Yglprg_vdp2_drawfb_cram_msb_color_col_f[]   = " if( msb == 0 ){ alpha = opaque; fbmode = 0;} \n ";
+const GLchar Yglprg_vdp2_drawfb_cram_msb_color_col_f[]   = " if( ret.msb == 0 ){ alpha = opaque; fbmode = 0;} \n ";
 
 
 const GLchar Yglprg_vdp2_sprite_palette_only[] =
