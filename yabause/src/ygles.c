c4598b513f3bb211e9ac09c4bfdc0bcfcaa266b2
FCare <francois.caron.perso@gmail.com>
2019-09-24 16:02:17 +0200

    Uniform can have a 0 value. Fix windows

--- yabause/src/ygles.c
+++ yabause/src/ygles.c
@@ -2728,7 +2728,7 @@ static int renderVDP1Level( YglLevel * level, int j, int* cprg, YglMatrix *mat, 
     }
     if( level->prg[j].currentQuad != 0 ) {
       ret = 1;
-      if(level->prg[j].mtxModelView>0) glUniformMatrix4fv(level->prg[j].mtxModelView, 1, GL_FALSE, (GLfloat*)&mat->m[0][0]);
+      if(level->prg[j].mtxModelView >= 0) glUniformMatrix4fv(level->prg[j].mtxModelView, 1, GL_FALSE, (GLfloat*)&mat->m[0][0]);
       glBindBuffer(GL_ARRAY_BUFFER, _Ygl->quads_buf);
       glBufferData(GL_ARRAY_BUFFER, level->prg[j].currentQuad * sizeof(float), level->prg[j].quads, GL_STREAM_DRAW);
       glVertexAttribPointer(level->prg[j].vertexp, 2, GL_FLOAT, GL_FALSE, 0, 0);
