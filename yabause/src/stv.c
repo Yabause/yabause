8fcd42c6a7cb80455473d7fb67ab0d8ef80b5ccb
Benjamin Siskoo <siskoo@tradu-france.com>
2020-01-06 11:52:19 +0100

    Kiwame stay with NV_1P

--- yabause/src/stv.c
+++ yabause/src/stv.c
@@ -1195,14 +1195,13 @@ const Game GameList[NB_STV_GAMES]={
    0,
    0,
    NULL,
-   NULL,
+   NV_1P,
    {
 	HEADER_BLOB,   "epr18737.13",    0x0000000, 0x0080000,
 	GAME_BYTE_BLOB, "epr18737.13",    0x0100000, 0x0080000,
 	GAME_WORD_BLOB, "mpr18738.2",    0x0400000, 0x0400000,
 	GAME_WORD_BLOB, "mpr18739.3",    0x0800000, 0x0400000,
 	GAME_WORD_BLOB, "mpr18740.4",    0x0c00000, 0x0200000,
-	EEPROM_BLOB,    "20091.bin",   0x0000, 0x0080,
         GAME_END, "", 0, 0
     },
   },
