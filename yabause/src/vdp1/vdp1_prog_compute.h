c9179711cf8703921128ea3a77cb07528c6754d7
FCare <francois.caron.perso@gmail.com>
2020-04-30 12:49:01 +0200

    Better horizontal upscaling

--- yabause/src/vdp1/vdp1_prog_compute.h
+++ yabause/src/vdp1/vdp1_prog_compute.h
@@ -218,7 +218,7 @@ SHADER_VERSION_COMPUTE
 "  for (uint i=0; i<step; i++) {\n"
 //A pixel shall be considered as part of an anti-aliased line if the distance of the pixel center to the line is shorter than (sqrt(0.5), which is the diagonal of the pixel
 //This represent the behavior of antialiasing as displayed in vdp1 spec.
-"    vec3 d = antiAliasedPoint(P+vec2(0.5), A, B);\n" //Get the projection of the point P to the line segment
+"    vec3 d = antiAliasedPoint(P+vec2(0.5)*upscale, A, B);\n" //Get the projection of the point P to the line segment
 "    if (distance(d.xy, P+vec2(0.5)) <= (length(upscale)/2.0)) {\n" //Test the distance between the projection on line and the center of the pixel
 "      float ux = d.z;\n" //u is the relative distance from first point to projected position
 "      float uy= (float(i)+0.5*upscale.y)/float(step);\n" //v is the ratio between the current line and the total number of lines
@@ -271,7 +271,7 @@ SHADER_VERSION_COMPUTE
 "  vec2 A = V0+(vec2(0.5)*upscale);\n"
 "  vec2 B = vec2(V1.x, V0.y)+(vec2(0.5)*upscale);\n"
 "  for (uint i=0; i<step; i++) {\n"
-"    vec3 d = antiAliasedPoint(P+vec2(0.5), A, B);\n"
+"    vec3 d = antiAliasedPoint(P+vec2(0.5)*upscale, A, B);\n"
 "    if (distance(d.xy, P+vec2(0.5)) <= (length(upscale)/2.0)) {\n"
 "      float ux= d.z;\n"
 "      float uy= (float(i))/float(step);\n"
@@ -343,7 +343,6 @@ SHADER_VERSION_COMPUTE
 
 "vec4 ReadSpriteColor(cmdparameter_struct pixcmd, vec2 uv, vec2 texel, out bool discarded){\n"
 "  vec4 color = vec4(0.0);\n"
-" if(uv.x==1.0) uv.x -= 0.5f/float(pixcmd.w);\n"
 " if ((pixcmd.flip & 0x2u) == 0x2u) {\n"
 "   uv.y += 0.5f/float(pixcmd.h);\n"
 " } else {\n"
@@ -354,7 +353,7 @@ SHADER_VERSION_COMPUTE
 " if ((pixcmd.flip & 0x2u) == 0x2u) posf = floor(posf);\n"
 " else posf = ceil(posf);\n"
 
-"  uint x = clamp(uint(uv.x*pixcmd.w), 0u, uint(pixcmd.w));\n"
+"  uint x = clamp(uint(uv.x*(pixcmd.w)), 0u, uint(pixcmd.w-1));\n"
 "  uint pos = clamp(uint(posf), 0u, uint(pixcmd.h-1))*pixcmd.w+x;\n"
 
 
