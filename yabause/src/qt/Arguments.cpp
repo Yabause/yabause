4d6c485bc5f600b5908cf424d0cfa04d081e82c6
Will-LWE <75226748+Will-LWE@users.noreply.github.com>
2020-12-01 22:09:57 +0100

    Support for language configuration from settings

--- yabause/src/qt/Arguments.cpp
+++ yabause/src/qt/Arguments.cpp
@@ -23,6 +23,7 @@ namespace Arguments
 	void autostart(const QString& param);
 	void binary(const QString& param);
 	void bios(const QString& param);
+	void syslangid(const QString& param);
 	void cdrom(const QString& param);
 	void fullscreen(const QString& param);
 	void help(const QString& param);
@@ -50,6 +51,7 @@ namespace Arguments
 		{ "-a",  "--autostart", NULL,       "Automatically start emulation.",                      1, autostart },
 		{ NULL,  "--binary=", "<FILE>[:ADDRESS]", "Use a binary file.",                           1, binary },
 		{ "-b",  "--bios=", "<BIOS>",       "Choose a bios file.",                                3, bios },
+		{ "-l",  "--language=", "<language>","Choose the system language: english, deutsch, french, spanish, italian, japanese",                     7, syslangid },
 		{ "-c",  "--cdrom=", "<CDROM>",     "Choose the cdrom device.",                           4, cdrom },
 		{ "-f",  "--fullscreen", NULL,      "Start the emulator in fullscreen.",                  5, fullscreen },
 		{ "-h",  "--help", NULL,            "Show this help and exit.",                           0, help },
@@ -62,8 +64,8 @@ namespace Arguments
 
 	void parse()
 	{
-		QVector<Option *> choosenOptions(7);
-		QVector<QString> params(7);
+		QVector<Option *> choosenOptions(8);
+		QVector<QString> params(8);
 
 		QStringList arguments = QApplication::arguments();
 		QStringListIterator argit(arguments);
@@ -90,7 +92,7 @@ namespace Arguments
 			}
 		}
 		
-		for(int i = 0;i < 7;i++)
+		for(int i = 0;i < 8;i++)
 		{
 			Option * option = choosenOptions[i];
 			if (option)
@@ -143,6 +145,17 @@ namespace Arguments
 		vs->setValue("General/Bios", param);
 	}
 
+	void syslangid(const QString& param)
+	{
+		VolatileSettings * vs = QtYabause::volatileSettings();
+		if (param.toLower() == "english") { vs->setValue("General/SystemLanguageID", 0); }
+		if (param.toLower() == "deutsch") { vs->setValue("General/SystemLanguageID", 1); }
+		if (param.toLower() == "french") { vs->setValue("General/SystemLanguageID", 2); }
+		if (param.toLower() == "spanish") { vs->setValue("General/SystemLanguageID", 3); }
+		if (param.toLower() == "italian") { vs->setValue("General/SystemLanguageID", 4); }
+		if (param.toLower() == "japanese") { vs->setValue("General/SystemLanguageID", 5); }
+	}
+	
 	void cdrom(const QString& param)
 	{
 		VolatileSettings * vs = QtYabause::volatileSettings();
