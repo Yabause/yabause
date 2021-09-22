12c869191cb3e7483ad63bf1d5a204d78ac996c5
barbudreadmon <barbudreadmon@users.noreply.github.com>
2021-09-22 11:28:54 +0200

    (libretro) we don't need the OSD messages

--- yabause/src/libretro/libretro.c
+++ yabause/src/libretro/libretro.c
@@ -1125,6 +1125,7 @@ bool retro_load_game(const struct retro_game_info *info)
 #endif
 
    ret = YabauseInit(&yinit);
+   OSDChangeCore(OSDCORE_DUMMY);
    YabauseSetDecilineMode(1);
 
    bool cheevos_supported                  = true;
