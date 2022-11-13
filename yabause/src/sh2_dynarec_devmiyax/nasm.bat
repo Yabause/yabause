edacf0ba06b4babc542401b1f83fdb17f9f71f5e
devMiyax <smiyaxdev@gmail.com>
2022-11-13 12:35:45 +0900

    dynlib for x86_64 just assemlbed

--- yabause/src/sh2_dynarec_devmiyax/nasm.bat
+++ yabause/src/sh2_dynarec_devmiyax/nasm.bat
@@ -1,4 +1,5 @@
 REM CMAKE does not support nasm. you need to run this command to generate dynalib.obj
 REM You can obtain nasm.exe from http://www.nasm.us/
 
-nasm.exe dynalib.asm -f win32 -O0 
+REM nasm.exe dynalib.asm -f win32 -O0 
+nasm.exe dynalib_x86_64.asm -f win32 -O0 
